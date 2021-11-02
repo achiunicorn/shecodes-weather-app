1c6e085ca6be9ae8acb3fa00d4bff5ea27ef295e
Amanda Chiu <87955310+achiunicorn@users.noreply.github.com>
2021-11-02 11:59:09 -0400

    Cleaned up Javascript

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,36 @@
+function searchCity(event) {
+  event.preventDefault();
+  let input = document.querySelector("#city-input");
+  let currentCity = document.querySelector("#current-city");
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
+
+  currentCity.innerHTML = input.value;
+  axios.get(apiURL).then(updateTemp);
+}
+
+function useCurrentLocation(event) {
+  navigator.geolocation.getCurrentPosition(searchCurrentCity);
+}
+
+function updateTemp(response) {
+  let changeTemp = document.querySelector("#changeTemp");
+  changeTemp.innerHTML = Math.round(response.data.main.temp);
+}
+
+function currentWeather(response) {
+  let searchedCity = document.querySelector("#current-city");
+
+  searchedCity.innerHTML = response.data.name;
+  updateTemp(response);
+}
+
+function searchCurrentCity(position) {
+  let apiKey = "bba7368be56623a87a536fec6a35c3b3";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(currentWeather);
+}
+
 let now = new Date();
 
 let weekDay = [
@@ -9,6 +42,7 @@ let weekDay = [
   "Friday",
   "Saturday",
 ];
+
 let day = weekDay[now.getDay()];
 
 let date = now.getDate();
@@ -27,6 +61,7 @@ let allMonths = [
   "November",
   "December",
 ];
+
 let month = allMonths[now.getMonth()];
 
 let year = now.getFullYear();
@@ -51,43 +86,9 @@ currentTime.innerHTML = `${hour}:${minutes}:${seconds}`;
 
 let apiKey = "bba7368be56623a87a536fec6a35c3b3";
 
-function searchCity(event) {
-  event.preventDefault();
-  let input = document.querySelector("#city-input");
-  console.log(input);
-  let searchedCity = document.querySelector("#current-city");
-
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=${apiKey}&units=metric`;
-
-  searchedCity.innerHTML = input.value;
-  axios.get(apiURL).then(updateTemp);
-}
-
-function updateTemp(response) {
-  let changeTemp = document.querySelector("#changeTemp");
-  changeTemp.innerHTML = Math.round(response.data.main.temp);
-}
-
 let searchHere = document.querySelector("#search-form");
-
-searchHere.addEventListener("submit", searchCity);
-
-function useCurrentLocation(event) {
-  navigator.geolocation.getCurrentPosition(searchCurrentCity);
-}
-
-function searchCurrentCity(position) {
-  let apiKey = "bba7368be56623a87a536fec6a35c3b3";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiURL).then(currentWeather);
-}
-
-function currentWeather(response) {
-  let searchedCity = document.querySelector("#current-city");
-  searchedCity.innerHTML = response.data.name;
-  updateTemp(response);
-}
 
 let findLocation = document.querySelector("#current-location");
 
+searchHere.addEventListener("submit", searchCity);
 findLocation.addEventListener("click", useCurrentLocation);
